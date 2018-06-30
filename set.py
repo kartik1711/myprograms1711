3ea4bf949e64072206b88e48803e32fd59d6f3a0
kartik1711 <40710490+kartik1711@users.noreply.github.com>
2018-06-30 12:02:38 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,15 @@
+#set operations
+A={1,2,3,4}
+B={4,5,6,7}
+print('length of A',len(A))
+print('length of B',len(B))
+print(2 in A)
+print(3 in B)
+print('UNION')
+print(A|B)
+print('INTERSECTION')
+print(A&B)
+print('SYMMETRIC DIFFERENCE')
+print(A^B)
+print('DIFFERENCE')
+print(A-B)
